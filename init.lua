7d2402eeb6fa513b5a10a908486292e9d2c519b0
Alex Christie <achris22@protonmail.com>
2024-08-04 15:43:52 +1000

    Updated theme and lualine

--- init.lua
+++ init.lua
@@ -331,6 +331,8 @@ require("lazy").setup({
 				tailwindcss = {},
 				cssls = {},
 				html = {},
+				julials = {},
+				zls = {},
 
 				lua_ls = {
 					-- cmd = {...},
@@ -524,6 +526,20 @@ require("lazy").setup({
 			require("mini.ai").setup({ n_lines = 500 })
 
 			require("mini.bracketed").setup()
+			-- Simple and easy statusline.
+			--  You could remove this setup call if you don't like it,
+			--  and try some other statusline plugin
+			local statusline = require("mini.statusline")
+			-- set use_icons to true if you have a Nerd Font
+			-- statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+			-- You can configure sections in the statusline by overriding their
+			-- default behavior. For example, here we set the section for
+			-- cursor location to LINE:COLUMN
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
 		end,
 	},
 	{ -- Highlight, edit, and navigate code
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,35 +1,38 @@
 return {
 
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
+	-- Change the name of the colorscheme plugin below, and then
+	-- change the command in the config to whatever the name of that colorscheme is.
+	--
+	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			-- vim.cmd.colorscheme 'tokyonight-night'
 
-      -- You can configure highlights by doing something like:
-      -- vim.cmd.hi 'Comment gui=none'
-    end,
-  },
-  {
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000,
-    config = true,
-    init = function()
-      -- vim.cmd.colorscheme 'gruvbox'
-    end,
-  },
-  {
-    'olimorris/onedarkpro.nvim',
-    priority = 1000, -- Ensure it loads first
-    init = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
+			-- You can configure highlights by doing something like:
+			-- vim.cmd.hi 'Comment gui=none'
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = true,
+		init = function()
+			-- vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+	{
+		"marko-cerovac/material.nvim",
+		priority = 1000,
+		config = function()
+			vim.g.material_style = "darker"
+		end,
+		init = function()
+			vim.cmd.colorscheme("material")
+		end,
+	},
 }
