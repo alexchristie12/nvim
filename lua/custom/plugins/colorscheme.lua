bfac91f59718db49d9a2c52c6274e495c93b23ba
Alex Christie <achris22@protonmail.com>
2024-10-04 13:11:54 +1000

    change colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -4,63 +4,56 @@ return {
 	-- change the command in the config to whatever the name of that colorscheme is.
 	--
 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			-- vim.cmd.colorscheme("tokyonight-night")
-
-			-- You can configure highlights by doing something like:
-			-- vim.cmd.hi 'Comment gui=none'
-		end,
-		config = function()
-			require("tokyonight").setup({
-				transparent = true,
-			})
-		end,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = true,
-		init = function()
-			-- vim.cmd.colorscheme("gruvbox")
-		end,
-	},
-	-- Lazy
-	{
-		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Ensure it loads first
-		init = function()
-			-- vim.cmd.colorscheme("onedark")
-		end,
-	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
+	-- 	init = function()
+	-- 		-- Load the colorscheme here.
+	-- 		-- Like many other themes, this one has different styles, and you could load
+	-- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+	-- 		-- vim.cmd.colorscheme("tokyonight-night")
+	--
+	-- 		-- You can configure highlights by doing something like:
+	-- 		-- vim.cmd.hi 'Comment gui=none'
+	-- 	end,
+	-- 	config = function()
+	-- 		require("tokyonight").setup({
+	-- 			transparent = true,
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	priority = 1000,
+	-- 	config = true,
+	-- 	init = function()
+	-- 		-- vim.cmd.colorscheme("gruvbox")
+	-- 	end,
+	-- },
+	-- -- Lazy
+	-- {
+	-- 	"olimorris/onedarkpro.nvim",
+	-- 	priority = 1000, -- Ensure it loads first
+	-- 	init = function()
+	-- 		-- vim.cmd.colorscheme("onedark")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	priority = 1000,
+	-- 	init = function()
+	-- 		-- vim.cmd.colorscheme("kanagawa-dragon")
+	-- 	end,
+	-- 	-- Lazy
+	-- },
 	{
 		"vague2k/vague.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		init = function()
-			-- vim.cmd.colorscheme("vague")
-		end,
+		priority = 1000,
 		config = function()
+			vim.cmd.colorscheme("vague")
 			require("vague").setup({
 				-- optional configuration here
-				transparent = true,
 			})
-		end,
-	},
-	{
-		"thimc/gruber-darker.nvim",
-		config = function()
-			require("gruber-darker").setup({
-				-- OPTIONAL
-				transparent = true, -- removes the background
-				-- underline = false, -- disables underline fonts
-				-- bold = false, -- disables bold fonts
-			})
-			vim.cmd.colorscheme("gruber-darker")
 		end,
 	},
 }
