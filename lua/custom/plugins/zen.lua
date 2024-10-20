54313d930ebd25c79783824fcd09a436fa5cac0b
Alex Christie <achris22@protonmail.com>
2024-10-21 08:25:31 +1000

    add zenmode plugin

---
+++ lua/custom/plugins/zen.lua
@@ -0,0 +1,14 @@
+return {
+	-- Lua
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		config = function()
+			vim.keymap.set("n", "<leader>zm", "<CMD>ZenMode<CR>")
+		end,
+	},
+}
